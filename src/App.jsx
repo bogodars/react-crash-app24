205f038c500d3841742499834e2d234580b52459
Bogodar Shmyglyk <bogodar@gmail.com>
2024-10-18 16:32:07 +0300

    Boilderplate with mapping json

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobsListings from './components/JobsListings'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <> 
+  <Navbar />
+  <Hero />
+  <HomeCards />
+  <JobsListings />   
+    </>
+  )
+}
+
+export default App
